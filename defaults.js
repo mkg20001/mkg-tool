bead7cd0544a6602102f8f00c711c52818f30495
mkg20001 <mkg20001@gmail.com>
2017-08-28 12:16:36 +0200

    Finish pex

--- defaults.js
+++ defaults.js
@@ -1,5 +1,6 @@
 module.exports = {
   bootstrap: [
     "/ip4/148.251.206.162/tcp/5235/ipfs/QmPSLYnmTUEE9JDS5udXENAYwrtYGs5EiErx5YAeTfCGnt/"
+    //"/ip4/127.0.0.1/tcp/5235/ipfs/QmWu2KoKnUKkTt35e8XzX3V3h6DwdPdJHZVAGCCyuFsYmB/"
   ]
 }
