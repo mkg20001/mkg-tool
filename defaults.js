4c8c43739957aceec80575095dcedf01cea0bc92
mkg20001 <mkg20001@gmail.com>
2018-01-24 19:02:10 +0100

    Add sentry for real

--- defaults.js
+++ defaults.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = {
   bootstrap: [
     '/ip4/136.243.31.32/tcp/5235/ipfs/QmNeteLZb2qE9UFxhD48Ds8ZrLYAoYRUKkFEgyT7KbSYwH'
--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 'use strict'
 
+const Raven = require('raven')
+Raven.config('https://80aa59f734b646e98a8d981528f99303:995f8b0f17284bdaaa1f9ebf9b523c76@sentry.io/277019').install()
+
 const node = require('.')
 
 node(require('./defaults'), (err, node) => {
