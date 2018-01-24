3ebf6563b099332cd5de0f62dbd7260455c12d25
mkg20001 <mkg20001@gmail.com>
2018-01-24 16:08:48 +0100

    Updates

--- defaults.js
+++ defaults.js
@@ -1,6 +1,5 @@
 module.exports = {
   bootstrap: [
-    '/ip4/148.251.206.162/tcp/5235/ipfs/QmPSLYnmTUEE9JDS5udXENAYwrtYGs5EiErx5YAeTfCGnt/',
-    '/ip4/136.243.31.32/tcp/5235/ipfs/QmNeteLZb2qE9UFxhD48Ds8ZrLYAoYRUKkFEgyT7KbSYwH/'
+    '/ip4/136.243.31.32/tcp/5235/ipfs/QmNeteLZb2qE9UFxhD48Ds8ZrLYAoYRUKkFEgyT7KbSYwH'
   ]
 }
